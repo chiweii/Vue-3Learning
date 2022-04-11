69d7a60e5457688784ab69c727f92acb56eaa3ea
chengwei <chiwei831221@gmail.com>
2022-04-11 17:40:10 +0800

    20220411

---
+++ lesson2/2-1-3Practice.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="app">
+        <h2>this vue file have to compiler to javascript then can executed</h2>
+        <div>Hello Vue!</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent'
+}
+</script>
+
+<style scoped>
+.app{
+    color: red;    
+}
+</style>
